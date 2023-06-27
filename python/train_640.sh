adf7f00ebc2f653f306b6f9902af1c80a198b288
puffyyy <ydl57913@163.com>
2023-06-27 15:39:14 +0800

    update

---
+++ python/train_640.sh
@@ -0,0 +1,2 @@
+CONFIG=/home/admin/workspace/yangdonglin/workplace/lidar-intensity/configs/train.reflect-l2.depth.rgb.label.weather_640.yml
+nohup python -u model_run.py $CONFIG > train_640.log 2>&1 &
