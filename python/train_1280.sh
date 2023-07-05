34786a8d8d2fe95986a0154bfc6df401a220af97
puffyyy <ydl57913@163.com>
2023-07-05 23:09:37 +0800

    new config

---
+++ python/train_1280.sh
@@ -0,0 +1,2 @@
+CONFIG=/home/admin/workspace/yangdonglin/workplace/lidar-intensity/configs/train.reflect-l2.depth.rgb.label.weather_1280.yml
+nohup python -u model_run.py $CONFIG > train_1280.log 2>&1 &
