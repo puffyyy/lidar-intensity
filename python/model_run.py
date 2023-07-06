d5392a325cc164172fd2f7af3b71ff2ae774709e
puffyyy <ydl57913@163.com>
2023-07-06 11:41:55 +0800

    update config

--- python/model_run.py
+++ python/model_run.py
@@ -1,16 +1,22 @@
 import sys
 
 import torch.utils.data as data
-
+import os
 import inten
 import otils as ot
 import torchutils as tu
-
+import datetime
+import shutil
 if __name__ == '__main__':
     config = ot.io.load_multi_yml(sys.argv[1])
+    config['store_dir'] = config['store_dir'] + datetime.datetime.today().strftime('%Y%m%d%H%M%S')
+    print()
+    os.makedirs(os.path.join(config['base_dir'], config['store_dir']), exist_ok=True)
+    shutil.copy2(sys.argv[1], os.path.join(config['base_dir'], config['store_dir'], os.path.basename(sys.argv[1])))
     if 'seed' in config:
         tu.seed_all(config['seed'])
     runner = inten.data.Runner(config)
+    print((config['train']))
     trn_dataset = data.DataLoader(inten.data.Dataset(config['train']), **config['train_loader'])
     val_dataset = data.DataLoader(inten.data.Dataset(config['val']), **config['val_loader'])
     if 'scheduler' in config:
--- python/trainv2_640.sh
+++ python/trainv2_640.sh
@@ -1,2 +1,2 @@
-CONFIG=/home/admin/workspace/yangdonglin/workplace/lidar-intensity/configs/train.reflect-l2.depth.rgb.label.weather.v2_640.yml
+CONFIG=/home/admin/workspace/yangdonglin/workplace/lidar-intensity/configs/train.reflect-l2.depth.rgb.label.weather_v2_640.yml
 nohup python -u model_run.py $CONFIG > train_640.log 2>&1 &
